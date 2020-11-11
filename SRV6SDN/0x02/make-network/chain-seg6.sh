a40e229baddbc12025bf9ffbaf789fcfa5d142e6
Shimosora <shimo_sora@outlook.jp>
2020-11-12 00:23:51 +0900

    add chain-seg6.sh

---
+++ SRV6SDN/0x02/make-network/chain-seg6.sh
@@ -0,0 +1,6 @@
+set -xe
+ip netns exec R1 ip rule add prio 100 from 10.1.0.11 table 100
+ip netns exec R1 ip route add default encap seg6 mode encap segs 2001:2::2,2001:4::1,2001:5::1,2001:3::2 dev net2 table 100
+#ip netns exec R1 ip route add default encap seg6 mode encap segs 2::,4::,5::,3:: dev net0 table 100
+ip netns exec R1 ip route list table 100
+
