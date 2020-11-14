0728d7aabc83e912d266499a4fc4e952eca0f392
Shimosora <shimo_sora@outlook.jp>
2020-11-14 17:54:21 +0900

    add C2,C3 route

--- SRV6SDN/0x02/make-network/chain-seg6.sh
+++ SRV6SDN/0x02/make-network/chain-seg6.sh
@@ -1,15 +1,27 @@
 set -xe
 
 ip netns exec R1 ip rule add prio 100 from 10.1.0.11 table 100
+ip netns exec R1 ip rule add prio 100 from 10.1.0.12 table 200
+ip netns exec R1 ip rule add prio 100 from 10.1.0.13 table 300
 ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:1::1,FF:2::1,F2::10 dev net2 table 100
+ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:2::1,FF:3::1,F2::10 dev net2 table 200
+ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:3::1,FF:1::1,F2::10 dev net2 table 300
 ip netns exec R1 ip route list table 100
 ip netns exec R1 ip route add ff:1::/64 via 2001:2::2 
 ip netns exec R1 ip route add ff:2::/64 via 2001:2::2 
 ip netns exec R1 ip route add ff:3::/64 via 2001:2::2 
+ip netns exec R1 sysctl net.ipv4.conf.net0.rp_filter=0
+ip netns exec R1 sysctl net.ipv4.conf.net1.rp_filter=0
+ip netns exec R1 sysctl net.ipv4.conf.net2.rp_filter=0
+ip netns exec R1 sysctl net.ipv4.conf.all.rp_filter=0
 
 ip netns exec R2 ip route add ff:1::/64 via 2001:3::2 
 ip netns exec R2 ip route add ff:2::/64 via 2001:3::2
 ip netns exec R2 ip route add ff:3::/64 via 2001:3::2
+ip netns exec R2 sysctl net.ipv4.conf.net0.rp_filter=0
+ip netns exec R2 sysctl net.ipv4.conf.net1.rp_filter=0
+ip netns exec R2 sysctl net.ipv4.conf.net2.rp_filter=0
+ip netns exec R2 sysctl net.ipv4.conf.all.rp_filter=0
 
 ip netns exec R3 ip route add ff:1::/64 via 2001:4::1
 ip netns exec R3 ip route add ff:2::/64 via 2001:5::1
---
+++ SRV6SDN/0x02/make-network/create.sh
@@ -0,0 +1,4 @@
+ sh create-netns.sh
+ sh create-R3.sh
+ sh seg6-setting.sh
+ sh chain-seg6.sh
