461d835181d21eaf34fd40e08f63ab1b454a4f5d
Shimosora <shimo_sora@outlook.jp>
2020-11-21 16:52:47 +0900

    fix create-netns.sh add add-policy.sh

---
+++ SRV6SDN/0x03/make-network/add-policy.sh
@@ -0,0 +1,17 @@
+set -xe
+
+#chain
+ip netns exec R1 ip rule add prio 100 from 10.1.0.11 table 100
+ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:1::1,FF:2::1,F2::10 dev net2 table 100
+ip netns exec R1 ip rule add prio 100 from 10.1.0.12 table 200
+ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:2::1,FF:3::1,F2::10 dev net2 table 200
+ip netns exec R1 ip rule add prio 100 from 10.1.0.13 table 300
+ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:3::1,FF:1::1,F2::10 dev net2 table 300
+
+ip netns exec R2 ip rule add prio 100 to 10.1.0.11 table 400
+ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:2::1,FF:1::1,F1::10 dev net2 table 400
+ip netns exec R2 ip rule add prio 100 to 10.1.0.12 table 500
+ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:3::1,FF:2::1,F1::10 dev net2 table 500
+ip netns exec R2 ip rule add prio 100 to 10.1.0.13 table 600
+ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:1::1,FF:3::1,F1::10 dev net2 table 600
+
--- SRV6SDN/0x03/make-network/create-netns.sh
+++ SRV6SDN/0x03/make-network/create-netns.sh
@@ -91,64 +91,16 @@ ip -n F2 link set net0 up
 ip -n F2 addr add 2001:5::1/64 dev net0
 ip -n F3 link set net0 up
 ip -n F3 addr add 2001:6::1/64 dev net0
-ip netns exec R3 sysctl net.ipv6.conf.all.forwarding=1
 
-#End.DT4
-ip netns exec R1 ip link add vrf10 type vrf table 10
-ip netns exec R1 ip link set vrf10 up
-ip netns exec R1 ip link set net1 vrf vrf10
-ip netns exec R1 ip link set net1 up
-ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs f2::10 dev net0 vrf vrf10
-ip netns exec R1 ip route add 169.254.0.10 dev vrf10
-ip netns exec R1 ip route add f1::10 encap seg6local action End.DX4 nh4 169.254.0.10 dev vrf10
 ip netns exec R1 ip route add f2::/64 via 2001:1::2
-sudo ip netns exec R1 sysctl net.ipv4.conf.all.forwarding=1
-sudo ip netns exec R1 sysctl net.ipv6.conf.all.forwarding=1
-sudo ip netns exec R1 sysctl net.ipv6.conf.all.seg6_enabled=1
- 
-ip netns exec R2 ip link add vrf10 type vrf table 10
-ip netns exec R2 ip link set vrf10 up
-ip netns exec R2 ip link set net1 vrf vrf10
-ip netns exec R2 ip link set net1 up
-ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs f1::10 dev net0 vrf vrf10
-ip netns exec R2 ip route add 169.254.0.10 dev vrf10
-ip netns exec R2 ip route add f2::10 encap seg6local action End.DX4 nh4 169.254.0.10 dev vrf10
-ip netns exec R2 ip route add f1::/64 via 2001:1::1
-sudo ip netns exec R2 sysctl net.ipv4.conf.all.forwarding=1
-sudo ip netns exec R2 sysctl net.ipv6.conf.all.forwarding=1
-sudo ip netns exec R2 sysctl net.ipv6.conf.all.seg6_enabled=1
-
-#chain
-ip netns exec R1 ip rule add prio 100 from 10.1.0.11 table 100
-ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:1::1,FF:2::1,F2::10 dev net2 table 100
-ip netns exec R1 ip rule add prio 100 from 10.1.0.12 table 200
-ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:2::1,FF:3::1,F2::10 dev net2 table 200
-ip netns exec R1 ip rule add prio 100 from 10.1.0.13 table 300
-ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:3::1,FF:1::1,F2::10 dev net2 table 300
-
-ip netns exec R2 ip rule add prio 100 to 10.1.0.11 table 400
-ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:2::1,FF:1::1,F1::10 dev net2 table 400
-ip netns exec R2 ip rule add prio 100 to 10.1.0.12 table 500
-ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:3::1,FF:2::1,F1::10 dev net2 table 500
-ip netns exec R2 ip rule add prio 100 to 10.1.0.13 table 600
-ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:1::1,FF:3::1,F1::10 dev net2 table 600
-
-#ip netns exec R1 ip route list table 100
 ip netns exec R1 ip route add ff:1::/64 via 2001:2::2
 ip netns exec R1 ip route add ff:2::/64 via 2001:2::2
 ip netns exec R1 ip route add ff:3::/64 via 2001:2::2
-ip netns exec R1 sysctl net.ipv4.conf.net0.rp_filter=0
-ip netns exec R1 sysctl net.ipv4.conf.net1.rp_filter=0
-ip netns exec R1 sysctl net.ipv4.conf.net2.rp_filter=0
-ip netns exec R1 sysctl net.ipv4.conf.all.rp_filter=0
 
+ip netns exec R2 ip route add f1::/64 via 2001:1::1
 ip netns exec R2 ip route add ff:1::/64 via 2001:3::2
 ip netns exec R2 ip route add ff:2::/64 via 2001:3::2
 ip netns exec R2 ip route add ff:3::/64 via 2001:3::2
-ip netns exec R2 sysctl net.ipv4.conf.net0.rp_filter=0
-ip netns exec R2 sysctl net.ipv4.conf.net1.rp_filter=0
-ip netns exec R2 sysctl net.ipv4.conf.net2.rp_filter=0
-ip netns exec R2 sysctl net.ipv4.conf.all.rp_filter=0
 
 ip netns exec R3 ip route add ff:1::/64 via 2001:4::1
 ip netns exec R3 ip route add ff:2::/64 via 2001:5::1
@@ -156,18 +108,67 @@ ip netns exec R3 ip route add ff:3::/64 via 2001:6::1
 ip netns exec R3 ip route add f2::/64 via 2001:3::1
 ip netns exec R3 ip route add f1::/64 via 2001:2::1
 
-ip netns exec F1 ip route add ff:1::1/128 encap seg6local action End dev net0
-ip netns exec F1 ip addr add ff:1::ffff/128 dev lo
+ip netns exec F1 ip route add default via 2001:4::2
+ip netns exec F2 ip route add default via 2001:5::2
+ip netns exec F3 ip route add default via 2001:6::2
+
+ip netns exec R1 sysctl net.ipv4.conf.net0.rp_filter=0
+ip netns exec R1 sysctl net.ipv4.conf.net1.rp_filter=0
+ip netns exec R1 sysctl net.ipv4.conf.net2.rp_filter=0
+ip netns exec R1 sysctl net.ipv4.conf.all.rp_filter=0
+sudo ip netns exec R1 sysctl net.ipv4.conf.all.forwarding=1
+sudo ip netns exec R1 sysctl net.ipv6.conf.all.forwarding=1
+sudo ip netns exec R1 sysctl net.ipv6.conf.all.seg6_enabled=1
+ip netns exec R2 sysctl net.ipv4.conf.net0.rp_filter=0
+ip netns exec R2 sysctl net.ipv4.conf.net1.rp_filter=0
+ip netns exec R2 sysctl net.ipv4.conf.net2.rp_filter=0
+ip netns exec R2 sysctl net.ipv4.conf.all.rp_filter=0
+sudo ip netns exec R2 sysctl net.ipv4.conf.all.forwarding=1
+sudo ip netns exec R2 sysctl net.ipv6.conf.all.forwarding=1
+sudo ip netns exec R2 sysctl net.ipv6.conf.all.seg6_enabled=1
+ip netns exec R3 sysctl net.ipv6.conf.all.forwarding=1
 ip netns exec F1 sysctl net.ipv6.conf.all.forwarding=1
 ip netns exec F1 sysctl net.ipv6.conf.all.seg6_enabled=1
-ip netns exec F1 ip route add default via 2001:4::2
-ip netns exec F2 ip route add ff:2::1/128 encap seg6local action End dev net0
-ip netns exec F2 ip addr add ff:2::ffff/128 dev lo
 ip netns exec F2 sysctl net.ipv6.conf.all.forwarding=1
 ip netns exec F2 sysctl net.ipv6.conf.all.seg6_enabled=1
-ip netns exec F2 ip route add default via 2001:5::2
-ip netns exec F3 ip route add ff:3::1/128 encap seg6local action End dev net0
-ip netns exec F3 ip addr add ff:3::ffff/128 dev lo
 ip netns exec F3 sysctl net.ipv6.conf.all.forwarding=1
 ip netns exec F3 sysctl net.ipv6.conf.all.seg6_enabled=1
-ip netns exec F3 ip route add default via 2001:6::2
+
+#End.DT4
+ip netns exec R1 ip link add vrf10 type vrf table 10
+ip netns exec R1 ip link set vrf10 up
+ip netns exec R1 ip link set net1 vrf vrf10
+ip netns exec R1 ip link set net1 up
+#ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs f2::10 dev net0 vrf vrf10
+ip netns exec R1 ip route add 169.254.0.10 dev vrf10
+ip netns exec R1 ip route add f1::10 encap seg6local action End.DX4 nh4 169.254.0.10 dev vrf10
+ 
+ip netns exec R2 ip link add vrf10 type vrf table 10
+ip netns exec R2 ip link set vrf10 up
+ip netns exec R2 ip link set net1 vrf vrf10
+ip netns exec R2 ip link set net1 up
+#ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs f1::10 dev net0 vrf vrf10
+ip netns exec R2 ip route add 169.254.0.10 dev vrf10
+ip netns exec R2 ip route add f2::10 encap seg6local action End.DX4 nh4 169.254.0.10 dev vrf10
+ip netns exec F1 ip route add ff:1::1/128 encap seg6local action End dev net0
+ip netns exec F1 ip addr add ff:1::ffff/128 dev lo
+ip netns exec F2 ip route add ff:2::1/128 encap seg6local action End dev net0
+ip netns exec F2 ip addr add ff:2::ffff/128 dev lo
+ip netns exec F3 ip route add ff:3::1/128 encap seg6local action End dev net0
+ip netns exec F3 ip addr add ff:3::ffff/128 dev lo
+
+#chain
+#ip netns exec R1 ip rule add prio 100 from 10.1.0.11 table 100
+#ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:1::1,FF:2::1,F2::10 dev net2 table 100
+#ip netns exec R1 ip rule add prio 100 from 10.1.0.12 table 200
+#ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:2::1,FF:3::1,F2::10 dev net2 table 200
+#ip netns exec R1 ip rule add prio 100 from 10.1.0.13 table 300
+#ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:3::1,FF:1::1,F2::10 dev net2 table 300
+#
+#ip netns exec R2 ip rule add prio 100 to 10.1.0.11 table 400
+#ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:2::1,FF:1::1,F1::10 dev net2 table 400
+#ip netns exec R2 ip rule add prio 100 to 10.1.0.12 table 500
+#ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:3::1,FF:2::1,F1::10 dev net2 table 500
+#ip netns exec R2 ip rule add prio 100 to 10.1.0.13 table 600
+#ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:1::1,FF:3::1,F1::10 dev net2 table 600
+
