1d2e2d3662ab24fefd378ef0655cb4c07f5fb882
Shimosora <shimo_sora@outlook.jp>
2020-11-21 15:41:04 +0900

    fix chaining order & PBR design

--- SRV6SDN/0x03/make-network/create-netns.sh
+++ SRV6SDN/0x03/make-network/create-netns.sh
@@ -120,12 +120,20 @@ sudo ip netns exec R2 sysctl net.ipv6.conf.all.seg6_enabled=1
 
 #chain
 ip netns exec R1 ip rule add prio 100 from 10.1.0.11 table 100
-ip netns exec R1 ip rule add prio 100 from 10.1.0.12 table 200
-ip netns exec R1 ip rule add prio 100 from 10.1.0.13 table 300
 ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:1::1,FF:2::1,F2::10 dev net2 table 100
+ip netns exec R1 ip rule add prio 100 from 10.1.0.12 table 200
 ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:2::1,FF:3::1,F2::10 dev net2 table 200
+ip netns exec R1 ip rule add prio 100 from 10.1.0.13 table 300
 ip netns exec R1 ip route add 10.2.0.0/24 encap seg6 mode encap segs FF:3::1,FF:1::1,F2::10 dev net2 table 300
-ip netns exec R1 ip route list table 100
+
+ip netns exec R2 ip rule add prio 100 to 10.1.0.11 table 400
+ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:2::1,FF:1::1,F1::10 dev net2 table 400
+ip netns exec R2 ip rule add prio 100 to 10.1.0.12 table 500
+ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:3::1,FF:2::1,F1::10 dev net2 table 500
+ip netns exec R2 ip rule add prio 100 to 10.1.0.13 table 600
+ip netns exec R2 ip route add 10.1.0.0/24 encap seg6 mode encap segs FF:1::1,FF:3::1,F1::10 dev net2 table 600
+
+#ip netns exec R1 ip route list table 100
 ip netns exec R1 ip route add ff:1::/64 via 2001:2::2
 ip netns exec R1 ip route add ff:2::/64 via 2001:2::2
 ip netns exec R1 ip route add ff:3::/64 via 2001:2::2
